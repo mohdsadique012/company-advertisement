7dc639a7371c371be7b78a28c8c0c30032260708
mohdsadique012 <mohdsadique012000@gmail.com>
2023-02-07 15:06:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+// import Navbarr from './Component/Navbar/Navbar'
+import Form from './Component/Login/Form'
+import "./App.css"
+import "bootstrap/dist/css/bootstrap.min.css"
+// import Footer from './Component/Footer/Footer'
+// import Mainpart from './Component/Mainpart'
+// import Main from "./Component/Main"
+// import Companylogo from './Component/Companylogo'
+// import Banner from './Component/Navbar/Banner'
+import Company_detail from './Company_detail'
+import { Routes, Route } from "react-router-dom";
+import Home from './Home';
+
+
+
+
 
 function App() {
+  //  <Navbarr />
+  //  <Banner />
+  //   <Main className="Mian" />
+  //   <Companylogo />
+  //   <Footer />
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    < >
+    <div className='appbody'>
+    <Routes>
+    <Route exact path="/" element={<Home />}></Route>
+    <Route exact path="/login" element={<Form />}></Route>
+    <Route exact path="/Company_detail" element={<Company_detail />}></Route>
+    
+    </Routes>
     </div>
-  );
+      
+    </>
+  )
 }
 
-export default App;
+export default App
